7efa34ce95cb8232a384c6ecdcf24548bd4df4db
Nathan Borror <nathan@nathanborror.com>
2025-06-22 14:41:24 -0700

    up/down navigation input history

--- Rabble/Views/MessageField.swift
+++ Rabble/Views/MessageField.swift
@@ -7,12 +7,27 @@ struct MessageField: View {
     let manager: ConnectionManager
     let submit: () -> Void
 
+    @State private var history: [String] = []
+    @State private var historyIndex: Int? = nil
+
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             Divider()
             HStack(alignment: .bottom) {
                 if manager.connected {
                     TextField("Message", text: $text, axis: .vertical)
+                        .onKeyPress { press in
+                            switch press.key {
+                            case .upArrow:
+                                handleHistoryBackward()
+                                return .handled
+                            case .downArrow:
+                                handleHistoryForward()
+                                return .handled
+                            default:
+                                return .ignored
+                            }
+                        }
                         .textFieldStyle(.plain)
                         .padding()
                         .onSubmit {
@@ -23,7 +38,7 @@ struct MessageField: View {
                     } label: {
                         Image(systemName: "arrow.up")
                             .padding(8)
-                            .background(.blue, in: .circle)
+                            .background(.blue, in: .rect(cornerRadius: 5))
                             .foregroundStyle(.white)
                             .padding(8)
                     }
@@ -44,6 +59,30 @@ struct MessageField: View {
     }
 
     func handleSubmit() {
+        history.append(text)
+        historyIndex = nil
+
         submit()
+    }
+
+    func handleHistoryBackward() {
+        guard !history.isEmpty else { return }
+        if let index = historyIndex {
+            historyIndex = (index > 0) ? index - 1 : historyIndex
+        } else {
+            historyIndex = history.count - 1
+        }
+        text = history[historyIndex!]
+    }
+
+    func handleHistoryForward() {
+        guard let index = historyIndex else { return }
+        if index < (history.count - 1) {
+            historyIndex = index + 1
+            text = history[historyIndex!]
+        } else {
+            historyIndex = nil
+            text = ""
+        }
     }
 }
