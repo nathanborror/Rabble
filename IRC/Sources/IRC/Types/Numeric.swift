9362d414434ed0d8157c6136bd29727c6b3437fb
Nathan Borror <nathan@nathanborror.com>
2025-06-29 12:41:37 -0700

    more numerics

--- IRC/Sources/IRC/Types/Numeric.swift
+++ IRC/Sources/IRC/Types/Numeric.swift
@@ -151,6 +151,15 @@ public enum Numeric: Codable, Equatable, Sendable {
     /// representing when the channel was created on the network.
     case RPL_CREATIONTIME(client: String, channel: String, creationtime: Date)
 
+    /// # 330: <client> <nick> <account> :is logged in as
+    /// Sent as a reply to the WHOIS command, this numeric indicates that the client with the nickname <nick> was authenticated as the owner of
+    /// <account>. This does not necessarily mean the user owns their current nickname, which is covered byRPL_WHOISREGNICK.
+    case RPL_WHOISACCOUNT(client: String, nick: String, account: String)
+
+    /// # 331: <client> <channel> :No topic is set
+    /// Sent to a client when joining a channel to inform them that the channel with the name <channel> does not have any topic set.
+    case RPL_NOTOPIC(client: String, channel: String)
+
     /// # 332: <client> <channel> :<topic>
     /// Sent to a client when joining the <channel> to inform them of the current topic of the channel.
     case RPL_TOPIC(client: String, channel: String, topic: String)
@@ -159,9 +168,49 @@ public enum Numeric: Codable, Equatable, Sendable {
     /// Sent to a client to let them know who set the topic (<nick>) and when they set it (<setat> is a unix timestamp). Sent after RPL_TOPIC (332).
     case RPL_TOPICWHOTIME(client: String, channel: String, nick: String, setat: String)
 
+    /// # 336: <client> <channel>
+    /// Sent to a client as a reply to the INVITE command when used with no parameter, to indicate a channel the client was invited to. This numeric should not
+    /// be confused with RPL_INVEXLIST (346), which is used as a reply to MODE.
+    case RPL_INVITELIST(client: String, channel: String)
+
+    /// # 337: <client> :End of /INVITE list
+    /// Sent as a reply to the INVITE command when used with no parameter, this numeric indicates the end of invitations a client received. This numeric should
+    /// not be confused with RPL_ENDOFINVEXLIST (347), which is used as a reply to MODE.
+    case RPL_ENDOFINVITELIST(client: String)
+
+    /// # 338: <client> <nick> :is actually ...
+    case RPL_WHOISACTUALLY
+
     /// # 341: <client> <nick> <channel>
     /// Sent as a reply to the INVITE command to indicate that the attempt was successful and the client with the nickname <nick> has been invited to <channel>.
     case RPL_INVITING(client: String, nick: String, channel: String)
+
+    /// # 346: <client> <channel> <mask>
+    /// Sent as a reply to the MODE command, when clients are viewing the current entries on a channel’s invite-exception list. <mask> is the given mask on the
+    /// invite-exception list. This numeric should not be confused with RPL_INVITELIST (336), which is used as a reply to INVITE. This numeric is sometimes
+    /// erroneously called RPL_INVITELIST, as this was the name used in RFC2812.
+    case RPL_INVEXLIST(client: String, channel: String, mask: String)
+
+    /// # 347: <client> <channel> :End of Channel Invite Exception List
+    /// Sent as a reply to the MODE command, this numeric indicates the end of a channel’s invite-exception list. This numeric should not be confused with
+    /// RPL_ENDOFINVITELIST (337), which is used as a reply to INVITE. This numeric is sometimes erroneously called RPL_ENDOFINVITELIST, as this was
+    /// the name used in RFC2812.
+    case RPL_ENDOFINVEXLIST(client: String, channel: String)
+
+    /// # 348: <client> <channel> <mask>
+    /// Sent as a reply to the MODE command, when clients are viewing the current entries on a channel’s exception list. <mask> is the given mask on the
+    /// exception list.
+    case RPL_EXCEPTLIST(client: String, channel: String, mask: String)
+
+    /// # 349: <client> <channel> :End of channel exception list
+    /// Sent as a reply to the MODE command, this numeric indicates the end of a channel’s exception list.
+    case RPL_ENDOFEXCEPTLIST(client: String, channel: String)
+
+    /// # 351: <client> <version> <server> :<comments>
+    /// Sent as a reply to the VERSION command, this numeric indicates information about the desired server. <version> is the name and version of the
+    /// software being used (including any revision information). <server> is the name of the server. <comments> may contain any further comments or
+    /// details about the specific version of the server.
+    case RPL_VERSION(client: String, version: String, server: String, comments: String)
 
     /// # 352: <client> <channel> <username> <host> <server> <nick> <flags> :<hopcount> <realname>
     /// Sent as a reply to the WHO command, this numeric gives information about the client with the nickname <nick>. Refer to RPL_WHOISUSER (311)
@@ -189,14 +238,47 @@ public enum Numeric: Codable, Equatable, Sendable {
     /// they have one. The last parameter of this numeric is a list of [prefix]<nick> pairs, delimited by a SPACE character (' ', 0x20).
     case RPL_NAMREPLY(client: String, symbol: String, channel: String, nicks: [String])
 
+    /// # 364: <client> <server1> <server2> :<hopcount> <server info>
+    /// Sent as a reply to the LINKS command, this numeric specifies servers <server1> and <server2> are linked together. For servers which follow a
+    /// spanning tree topology, <server2> is the closest to the client. <server info> is a string containing a description of that server.
+    case RPL_LINKS(client: String, server1: String, server2: String, hopcount: Int, serverInfo: String)
+
+    /// # 365: <client> * :End of /LINKS list
+    /// Sent as a reply to the LINKS command, this numeric specifies the end of a list of channel member names.
+    case RPL_ENDOFLINKS(client: String)
+
     /// # 366: <client> <channel> :End of /NAMES list
     /// Sent as a reply to the NAMES command, this numeric specifies the end of a list of channel member names.
     case RPL_ENDOFNAMES(client: String, channel: String, text: String)
+
+    /// # 367: <client> <channel> <mask> [<who> <set-ts>]
+    /// Sent as a reply to the MODE command, when clients are viewing the current entries on a channel’s ban list. <mask> is the given mask on the ban list.
+    /// <who> and <set-ts> are optional and MAY be included in responses. <who> is either the nickname or nickmask of the client that set the ban, or a
+    /// server name, and <set-ts> is the UNIX timestamp of when the ban was set.
+    case RPL_BANLIST(client: String, channel: String, mask: String)
+
+    /// # 368: <client> <channel> :End of channel ban list
+    /// Sent as a reply to the MODE command, this numeric indicates the end of a channel’s ban list.
+    case RPL_ENDOFBANLIST(client: String, channel: String)
+
+    /// # 369: <client> <nick> :End of WHOWAS
+    /// Sent as a reply to the WHOWAS command, this numeric indicates the end of a WHOWAS reponse for the nickname <nick>. This numeric is sent after
+    /// all other WHOWAS response numerics have been sent to the client.
+    case RPL_ENDOFWHOWAS(client: String, nick: String)
+
+    /// # 371: <client> :<string>
+    /// Sent as a reply to the INFO command, this numeric returns human-readable information describing the server: e.g. its version, list of authors and
+    /// contributors, and any other miscellaneous information which may be considered to be relevant.
+    case RPL_INFO(client: String, text: String)
 
     /// # 372: <client> :<line of the motd>
     /// When sending the Message of the Day to the client, servers reply with each line of the MOTD as this numeric. MOTD lines MAY be wrapped to 80
     /// characters by the server.
     case RPL_MOTD(client: String, text: String)
+
+    /// # 374: <client> :End of INFO list
+    /// Indicates the end of an INFO response.
+    case RPL_ENDOFINFO(client: String)
 
     /// # 375: <client> :- <server> Message of the day -
     /// Indicates the start of the Message of the Day to the client. The text used in the last param of this message may vary, and SHOULD be displayed as-is
@@ -212,15 +294,151 @@ public enum Numeric: Codable, Equatable, Sendable {
     case RPL_HOSTHIDDEN
 
     // Errors
+
+    /// # 400: <client> <command>{ <subcommand>} :<info>
+    /// Indicates that the given command/subcommand could not be processed. <subcommand> may repeat for more specific subcommands.
+    case ERR_UNKNOWNERROR
+
+    /// # 401: <client> <nickname> :No such nick/channel
+    /// Indicates that no client can be found for the supplied nickname. The text used in the last param of this message may vary.
     case ERR_NOSUCHNICK(client: String, nick: String, text: String)
+
+    /// # 402:
+    case ERR_NOSUCHSERVER
+
+    /// # 403:
     case ERR_NOSUCHCHANNEL(client: String, channel: String, text: String)
+
+    /// # 404:
+    case ERR_CANNOTSENDTOCHAN
+
+    /// # 405:
+    case ERR_TOOMANYCHANNELS
+
+    /// # 406:
+    case ERR_WASNOSUCHNICK
+
+    /// # 409:
+    case ERR_NOORIGIN
+
+    /// # 411:
+    case ERR_NORECIPIENT
+
+    /// # 412:
+    case ERR_NOTEXTTOSEND
+
+    /// # 417:
+    case ERR_INPUTTOOLONG
+
+    /// # 421:
+    case ERR_UNKNOWNCOMMAND
+
+    /// # 422:
+    case ERR_NOMOTD
+
+    /// # 431:
+    case ERR_NONICKNAMEGIVEN
+
+    /// # 432:
+    case ERR_ERRONEUSNICKNAME
+
+    /// # 433:
+    case ERR_NICKNAMEINUSE
+
+    /// # 436:
+    case ERR_NICKCOLLISION
+
+    /// # 441:
+    case ERR_USERNOTINCHANNEL
+
+    /// # 442:
     case ERR_NOTONCHANNEL(client: String, channel: String, text: String)
+
+    /// # 443:
+    case ERR_USERONCHANNEL
+
+    /// # 451:
+    case ERR_NOTREGISTERED
+
+    /// # 461:
+    case ERR_NEEDMOREPARAMS
+
+    /// # 462:
+    case ERR_ALREADYREGISTERED
+
+    /// # 464:
+    case ERR_PASSWDMISMATCH
+
+    /// # 465:
+    case ERR_YOUREBANNEDCREEP
+
+    /// # 471:
+    case ERR_CHANNELISFULL
+
+    /// # 472:
+    case ERR_UNKNOWNMODE
+
+    /// # 473:
+    case ERR_INVITEONLYCHAN
+
+    /// # 474:
+    case ERR_BANNEDFROMCHAN
+
+    /// # 475:
+    case ERR_BADCHANNELKEY
+
+    /// # 476:
+    case ERR_BADCHANMASK
+
+    /// # 481:
+    case ERR_NOPRIVILEGES
+
+    /// # 482:
     case ERR_CHANOPRIVSNEEDED(client: String, channel: String, text: String)
+
+    /// # 483:
+    case ERR_CANTKILLSERVER
+
+    /// # 491:
+    case ERR_NOOPERHOST
+
+    /// # 501:
+    case ERR_UMODEUNKNOWNFLAG
+
+    /// # 502:
+    case ERR_USERSDONTMATCH
+
+    /// # 524:
+    case ERR_HELPNOTFOUND
+
+    /// # 525:
+    case ERR_INVALIDKEY
+
+    /// # 691:
+    case ERR_STARTTLS
+
+    /// # 696:
+    case ERR_INVALIDMODEPARAM
+
+    /// # 723:
+    case ERR_NOPRIVS
+
+    /// # 902:
+    case ERR_NICKLOCKED
 
     /// # 904: <client> :SASL authentication failed
     /// This numeric indicates that SASL authentication failed because of invalid credentials or other errors not explicitly mentioned by other numerics. For more
     /// information on this numeric, see the IRCv3 sasl-3.1 extension. The text used in the last param of this message varies wildly.
     case ERR_SASLFAIL(client: String, text: String)
+
+    /// # 905:
+    case ERR_SASLTOOLONG
+
+    /// # 906:
+    case ERR_SASLABORTED
+
+    /// # 907:
+    case ERR_SASLALREADY
 
     public init?(_ string: String, params: [String]) {
         guard let code = UInt16(string) else {
@@ -316,15 +534,44 @@ public enum Numeric: Codable, Equatable, Sendable {
             guard let interval = TimeInterval(params[2]) else { return nil }
             let date = Date(timeIntervalSince1970: interval)
             self = .RPL_CREATIONTIME(client: params[0], channel: params[1], creationtime: date)
+        case 330:
+            guard params.count >= 3 else { return nil }
+            self = .RPL_WHOISACCOUNT(client: params[0], nick: params[1], account: params[2])
+        case 331:
+            guard params.count >= 2 else { return nil }
+            self = .RPL_NOTOPIC(client: params[0], channel: params[1])
         case 332:
             guard params.count >= 3 else { return nil }
             self = .RPL_TOPIC(client: params[0], channel: params[1], topic: params[2])
         case 333:
             guard params.count >= 4 else { return nil }
             self = .RPL_TOPICWHOTIME(client: params[0], channel: params[1], nick: params[2], setat: params[3])
+        case 336:
+            guard params.count >= 2 else { return nil }
+            self = .RPL_INVITELIST(client: params[0], channel: params[1])
+        case 337:
+            guard params.count >= 1 else { return nil }
+            self = .RPL_ENDOFINVITELIST(client: params[0])
+        case 338:
+            self = .RPL_WHOISACTUALLY
         case 341:
             guard params.count >= 3 else { return nil }
             self = .RPL_INVITING(client: params[0], nick: params[1], channel: params[2])
+        case 346:
+            guard params.count >= 3 else { return nil }
+            self = .RPL_INVEXLIST(client: params[0], channel: params[1], mask: params[2])
+        case 347:
+            guard params.count >= 2 else { return nil }
+            self = .RPL_ENDOFINVEXLIST(client: params[0], channel: params[1])
+        case 348:
+            guard params.count >= 3 else { return nil }
+            self = .RPL_EXCEPTLIST(client: params[0], channel: params[1], mask: params[2])
+        case 349:
+            guard params.count >= 2 else { return nil }
+            self = .RPL_ENDOFEXCEPTLIST(client: params[0], channel: params[1])
+        case 351:
+            guard params.count >= 4 else { return nil }
+            self = .RPL_VERSION(client: params[0], version: params[1], server: params[2], comments: params[3])
         case 352:
             guard params.count >= 8 else { return nil }
             self = .RPL_WHOREPLY(client: params[0], channel: params[1], ident: params[2], hostname: params[3], server: params[4], nick: params[5], flags: params[6], name: params[7])
@@ -332,12 +579,33 @@ public enum Numeric: Codable, Equatable, Sendable {
             guard params.count >= 4 else { return nil }
             let nicks = params[3].split(separator: " ").map(String.init)
             self = .RPL_NAMREPLY(client: params[0], symbol: params[1], channel: params[2], nicks: nicks)
+        case 364:
+            guard params.count >= 5 else { return nil }
+            self = .RPL_LINKS(client: params[0], server1: params[1], server2: params[2], hopcount: Int(params[3]) ?? 0, serverInfo: params[4])
+        case 365:
+            guard params.count >= 1 else { return nil }
+            self = .RPL_ENDOFLINKS(client: params[0])
         case 366:
             guard params.count >= 3 else { return nil }
             self = .RPL_ENDOFNAMES(client: params[0], channel: params[1], text: params[2])
+        case 367:
+            guard params.count >= 3 else { return nil }
+            self = .RPL_BANLIST(client: params[0], channel: params[1], mask: params[2])
+        case 368:
+            guard params.count >= 2 else { return nil }
+            self = .RPL_ENDOFBANLIST(client: params[0], channel: params[1])
+        case 369:
+            guard params.count >= 2 else { return nil }
+            self = .RPL_ENDOFWHOWAS(client: params[0], nick: params[1])
+        case 371:
+            guard params.count >= 2 else { return nil }
+            self = .RPL_INFO(client: params[0], text: params[1])
         case 372:
             guard params.count >= 2 else { return nil }
             self = .RPL_MOTD(client: params[0], text: params[1])
+        case 374:
+            guard params.count >= 1 else { return nil }
+            self = .RPL_ENDOFINFO(client: params[0])
         case 375:
             guard params.count >= 2 else { return nil }
             self = .RPL_MOTDSTART(client: params[0], text: params[1])
@@ -352,18 +620,100 @@ public enum Numeric: Codable, Equatable, Sendable {
         case 401:
             guard params.count >= 3 else { return nil }
             self = .ERR_NOSUCHNICK(client: params[0], nick: params[1], text: params[2])
+        case 402:
+            self = .ERR_NOSUCHSERVER
         case 403:
             guard params.count >= 3 else { return nil }
             self = .ERR_NOSUCHCHANNEL(client: params[0], channel: params[1], text: params[2])
+        case 404:
+            self = .ERR_CANNOTSENDTOCHAN
+        case 405:
+            self = .ERR_TOOMANYCHANNELS
+        case 406:
+            self = .ERR_WASNOSUCHNICK
+        case 409:
+            self = .ERR_NOORIGIN
+        case 411:
+            self = .ERR_NORECIPIENT
+        case 412:
+            self = .ERR_NOTEXTTOSEND
+        case 417:
+            self = .ERR_INPUTTOOLONG
+        case 421:
+            self = .ERR_UNKNOWNCOMMAND
+        case 422:
+            self = .ERR_NOMOTD
+        case 431:
+            self = .ERR_NONICKNAMEGIVEN
+        case 432:
+            self = .ERR_ERRONEUSNICKNAME
+        case 433:
+            self = .ERR_NICKNAMEINUSE
+        case 436:
+            self = .ERR_NICKCOLLISION
+        case 441:
+            self = .ERR_USERNOTINCHANNEL
         case 442:
             guard params.count >= 3 else { return nil }
             self = .ERR_NOTONCHANNEL(client: params[0], channel: params[1], text: params[2])
+        case 443:
+            self = .ERR_USERONCHANNEL
+        case 451:
+            self = .ERR_NOTREGISTERED
+        case 461:
+            self = .ERR_NEEDMOREPARAMS
+        case 462:
+            self = .ERR_ALREADYREGISTERED
+        case 464:
+            self = .ERR_PASSWDMISMATCH
+        case 465:
+            self = .ERR_YOUREBANNEDCREEP
+        case 471:
+            self = .ERR_CHANNELISFULL
+        case 472:
+            self = .ERR_UNKNOWNMODE
+        case 473:
+            self = .ERR_INVITEONLYCHAN
+        case 474:
+            self = .ERR_BANNEDFROMCHAN
+        case 475:
+            self = .ERR_BADCHANNELKEY
+        case 476:
+            self = .ERR_BADCHANMASK
+        case 481:
+            self = .ERR_NOPRIVILEGES
         case 482:
             guard params.count >= 3 else { return nil }
             self = .ERR_CHANOPRIVSNEEDED(client: params[0], channel: params[1], text: params[2])
+        case 483:
+            self = .ERR_CANTKILLSERVER
+        case 491:
+            self = .ERR_NOOPERHOST
+        case 501:
+            self = .ERR_UMODEUNKNOWNFLAG
+        case 502:
+            self = .ERR_USERSDONTMATCH
+        case 524:
+            self = .ERR_HELPNOTFOUND
+        case 525:
+            self = .ERR_INVALIDKEY
+        case 691:
+            self = .ERR_STARTTLS
+        case 696:
+            self = .ERR_INVALIDMODEPARAM
+        case 723:
+            self = .ERR_NOPRIVS
+        case 902:
+            self = .ERR_NICKLOCKED
         case 904:
             guard params.count >= 2 else { return nil }
             self = .ERR_SASLFAIL(client: params[0], text: params[1])
+        case 905:
+            self = .ERR_SASLTOOLONG
+        case 906:
+            self = .ERR_SASLABORTED
+        case 907:
+            self = .ERR_SASLALREADY
         default:
             return nil
         }
